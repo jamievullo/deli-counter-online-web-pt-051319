9ca368d09abc1aa5effe29148f4771ad7e62ee0e
Jamie Vullo <jvullo@protonmail.com>
2020-07-19 18:15:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
     if(katz_deli.empty?)
       puts "The line is currently empty."
     else
-      i = 1
       katz_deli.each do |name|
+        i = 1
         puts "#{i} #{name}"
         i++
       end
