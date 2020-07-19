35b50e4b759267ca2edce6c854dfcd43e931673e
Jamie Vullo <jvullo@protonmail.com>
2020-07-19 18:08:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 
-def line
-  katz_deli = []
+katz_deli = []
+
+def line(katz-deli)
   katz_deli.each do |name|
     if(katz_deli.isEmpty?)
       puts "The line is currently empty"
