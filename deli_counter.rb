46f0a17c94cfef2d388a0a68a0ee549d831255b3
Jamie Vullo <jvullo@protonmail.com>
2020-07-19 18:21:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
       puts "The line is currently empty."
     else
         i = 1
-        lineString = "The name is currently:"
+        lineString = "The line is currently:"
       katz_deli.each do |name|
         lineString += " #{i}. #{name}"
         i += 1
